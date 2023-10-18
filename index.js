06fb41864da558fdaa6e365e8d601576c2dbc33b
profissao-dev-backend-javascript <148285611+profissao-dev-backend-javascript@users.noreply.github.com>
2023-10-18 22:07:42 +0000

    Implement endpoint `Create` `[POST] /items`

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+// Sinalizar para o Express que o corpo
+// das requisições estará sempre em JSON
+app.use(express.json())
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
@@ -16,6 +20,18 @@ const items = ["Java", "Android", "Kotlin", "Express", "NestJS"]
 // READ ALL - [GET] /items
 app.get("/items", function (req, res) {
   res.send(items)
+})
+
+// CREATE - [POST] /items
+app.post("/items", function (req, res) {
+  // Extraio a informação do corpo da requisição
+  const item = req.body.name
+
+  // Insiro ela na lista
+  items.push(item)
+
+  // Enviamos uma mensagem de sucesso
+  res.send("Item created successfully.")
 })
 
 app.listen(3000, function () {
