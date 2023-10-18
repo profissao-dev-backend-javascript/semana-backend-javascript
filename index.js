ce494deb8f3b88906179b4ad811aa49bd9670717
profissao-dev-backend-javascript <148285611+profissao-dev-backend-javascript@users.noreply.github.com>
2023-10-18 21:52:07 +0000

    Implement endpoint `Read All`

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.get('/oi', function (req, res) {
   res.send('Olá, mundo!')
 })
 
+// CRUD de lista de DevMon
+
+const items = ["Java", "Android", "Kotlin", "Express", "NestJS"]
+
+// READ ALL - [GET] /items
+app.get("/items", function (req, res) {
+  res.send(items)
+})
+
 app.listen(3000, function () {
   console.log("App running on http://localhost:3000")
 })
