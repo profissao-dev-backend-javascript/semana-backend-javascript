19205b91f51ec494ace7cae247a71fb052dc5dad
profissao-dev-backend-javascript <148285611+profissao-dev-backend-javascript@users.noreply.github.com>
2023-10-18 22:38:47 +0000

    Add comments

--- index.js
+++ index.js
@@ -55,12 +55,15 @@ app.post("/items", function (req, res) {
   // Extraio a informação do corpo da requisição
   const item = req.body
 
+  // Validamos o corpo da requisição, garantindo que tem
+  // as propriedades corretas
   if (!item || !item.name || !item.imageUrl) {
     return res.status(400).send({
       message: "name & imageUrl are required."
     })
   }
 
+  // Calculamos o novo ID a partir da quantidade de itens na lista
   item.id = items.length + 1
 
   // Insiro ela na lista
@@ -83,6 +86,8 @@ app.put("/items/:id", function (req, res) {
     return elemento.id === id
   })
 
+  // Pegamos todas as propriedades do newItem e atualizamos
+  // na lista, mantendo o ID atual
   items[index] = {
     ...newItem,
     id,
@@ -97,6 +102,7 @@ app.delete("/items/:id", function (req, res) {
   // Acessamos o parâmetro de rota e corrigimos o índice
   const id = +req.params.id
 
+  // Buscamos o índice da lista para o elemento com o ID passado
   const index = items.findIndex(function (element) {
     return elemento.id === id
   })
